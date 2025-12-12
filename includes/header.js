9eae492b1935befd69790515da964c4eba05cd2c
Quinten-vd <quinten.vandommelen@student.gibbrasschaat.be>
2025-12-12 15:36:07 +0100

    Update header.js

--- includes/header.js
+++ includes/header.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+  fetch("/solo-cup/includes/header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header-placeholder").innerHTML = data;
 
+    // titel uit body hallen
+    const title = document.body.dataset.title;
+    const titleElement = document.getElementById("page title");
+    if (title && titleElement) {
+      titleElement.textContext = title;
+    }
+
+    // logo uit body halen
+    const logoSrc = document.body.dataSet.logo;
+    const logoElement = document.getElementById("sile-logo");
+    if (logpSrc && logoElement) {
+      logoElement.src = logoSrc;
+    }
+  });
+});
